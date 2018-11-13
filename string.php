523bf5064e7893a0879768e20c23480fdd86bf77
jouiniwael <44771623+jouiniwael@users.noreply.github.com>
2018-11-13 10:26:06 +0100

    Add files via upload

---
+++ string.php
@@ -0,0 +1,16 @@
+<?php
+function strlen (){
+    $str='';
+    for($i=0;$i<strlen($str);){
+        echo $str[$i].<'br'>;
+    }
+    echo 'strnlk';
+}
+
+<?php
+function ucwords (){
+    $str='';
+    echo ucwords($str);
+}
+?>
+
