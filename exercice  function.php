ad6959be7b54ae23f64c3c1ab1ec3bac6b65305e
jouiniwael <44771623+jouiniwael@users.noreply.github.com>
2018-11-13 10:22:31 +0100

    Add files via upload

---
+++ exercice  function.php
@@ -0,0 +1,15 @@
+<?php
+
+ function compare($i,$j){
+     if($i<$j){
+         echo "$j est plus grand que $i";}
+         elseif ($i>$j){
+         echo"$i est plus grand que $j";}
+         else{
+             echo"$i et $j sont égaux";
+
+         }
+     
+ }
+
+ compare(9,7);
