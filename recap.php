551965c67b9a2c1b312a3ef0659b9106b0373b4b
jouiniwael <44771623+jouiniwael@users.noreply.github.com>
2018-11-18 13:39:37 +0100

    Create recap.php

---
+++ recap.php
@@ -0,0 +1,60 @@
+<?php
+$cin = isset($_POST['cin']) ? $_POST['cin'] : NULL;
+$nom = isset($_POST['nom']) ? $_POST['nom'] : NULL;
+$prenom = isset($_POST['prenom']) ? $_POST['prenom'] : NULL;
+$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : NULL;
+$kaskrout_kifech = isset($_POST['kaskrout_kifech']) ? $_POST['kaskrout_kifech'] : NULL;
+$harissa = isset($_POST['harissa']) ? $_POST['harissa'] : NULL;
+$salade = isset($_POST['salade']) ? $_POST['salade'] : NULL;
+$mayo = isset($_POST['mayo']) ? $_POST['mayo'] : NULL;
+$ketchup = isset($_POST['ketchup']) ? $_POST['ketchup'] : NULL;
+$adresse = isset($_POST['adresse']) ? $_POST['adresse'] : NULL;
+?>
+
+<?php
+$cin = isset($_POST['cin']) ? $_POST['cin'] : NULL;
+$nom = isset($_POST['nom']) ? $_POST['nom'] : NULL;
+$prenom = isset($_POST['prenom']) ? $_POST['prenom'] : NULL;
+$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : NULL;
+$kaskrout_kifech = isset($_POST['kaskrout_kifech']) ? $_POST['kaskrout_kifech'] : NULL;
+$harissa = isset($_POST['harissa']) ? $_POST['harissa'] : NULL;
+$salade = isset($_POST['salade']) ? $_POST['salade'] : NULL;
+$mayo = isset($_POST['mayo']) ? $_POST['mayo'] : NULL;
+$ketchup = isset($_POST['ketchup']) ? $_POST['ketchup'] : NULL;
+$adresse = isset($_POST['adresse']) ? $_POST['adresse'] : NULL;
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>recap</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+</head>
+<body class="container text-monospace  bg-light ">
+<div class="col-md-12 border bg-info text-white text-uppercase" >
+    <?php
+    echo'nom:'.$nom;echo'<br><br>';
+    echo'prenom'.$prenom;echo'<br><br>';
+    echo'cin'.$cin;echo'<br><br>';
+
+
+    echo' nombres de sandwich :'.$nombre; echo'<br><br>';
+    echo ' choix :<br><br>';
+    echo 'avec : ';
+    echo $harissa;echo' ';
+    echo $salade;echo' ';
+    echo $mayo;echo' ';
+    echo $ketchup;echo'<br><br>';
+    echo 'adresse du client :' .$adresse.'<br><br>';
+    $res=$nombre/10;
+    $res1=(int)$res;
+    $prix=($nombre*4)-($res1*4);
+    echo' vous avez :'.$res1.'gratuit <br><br>';
+    echo ' vous paierais seulement:'.$prix .'dt<br><br>';
+    echo ' a bientot';
+    ?>
+</div>
+</body>
+
+
+</html>
